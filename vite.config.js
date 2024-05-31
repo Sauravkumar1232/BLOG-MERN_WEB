517e490fa0811b2539f07f5d4bb180f9e92190f6
Sauravkumar1232 <97805415+Sauravkumar1232@users.noreply.github.com>
2024-05-31 20:39:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+ 
+//   build: {
+//     outDir: "dist",
+//   },
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
- 
-  build: {
-    outDir: "dist",
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://blog-mern-api-tck7.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
+
