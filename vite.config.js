bd64d799b51b0cf2be28ef187a454ccf576ea0cc
Sauravkumar1232 <97805415+Sauravkumar1232@users.noreply.github.com>
2024-05-31 20:40:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,8 @@
 //   },
 // });
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
