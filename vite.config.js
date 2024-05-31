200bdb8a19dcbfccecca4d8fe3f6a6c520007d3c
Sauravkumar1232 <97805415+Sauravkumar1232@users.noreply.github.com>
2024-05-31 21:20:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
     proxy: {
       '/api': {
         target: 'https://blog-mern-api-tck7.onrender.com',
