c32ca1ef9e203cdd0fcdd5bf6425268b9c176028
Sauravkumar1232 <97805415+Sauravkumar1232@users.noreply.github.com>
2024-06-01 15:40:24 +0530

    Update helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://localhost:3000";
-export const BASE_URL = "https://blog-mern-api-tck7.onrender.com";
+export const BASE_URL = "https://blog-mern-api-bbgx.onrender.com";
