c60255f5385603ef75835a15fd3b753cf4c2656b
Sauravkumar1232 <97805415+Sauravkumar1232@users.noreply.github.com>
2024-06-02 11:02:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,29 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
- 
-//   build: {
-//     outDir: "dist",
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    // proxy: {
-    //   '/api': {
-    //     target: 'https://blog-mern-api-tck7.onrender.com',
-    //     changeOrigin: true,
-    //     secure: false,
-    //   },
-    // },
+ 
+  build: {
+    outDir: "dist",
   },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     // proxy: {
+//     //   '/api': {
+//     //     target: 'https://blog-mern-api-tck7.onrender.com',
+//     //     changeOrigin: true,
+//     //     secure: false,
+//     //   },
+//     // },
+//   },
+// });
 
